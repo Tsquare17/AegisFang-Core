3c1a0d1b0dc685659e950827e9b5ac5d51bce113
Trevor Thompson <tsquare1717@gmail.com>
2020-02-05 19:56:20 -0500

    Add select and insert tests.

--- tests/Database/QueryTest.php
+++ tests/Database/QueryTest.php
@@ -3,25 +3,59 @@
 namespace AegisFang\Tests;
 
 use AegisFang\Database\Query;
+use AegisFang\Database\Table\Blueprint;
+use AegisFang\Database\Table\Builder;
 use PHPUnit\Framework\TestCase;
 use AegisFang\Database\Connection;
 
 class QueryTest extends TestCase
 {
-    protected $pdo;
+    protected Connection $conn;
 
-    protected $query;
+    protected Query $query;
 
     public function setUp(): void
     {
-        $this->pdo = new Connection();
+        $blueprint = new Blueprint();
+        $blueprint->id('test_id');
+        $blueprint->string('test_string', 100);
+        $blueprint->int('test_int', true, true);
+        $table = new Builder('test_table', $blueprint);
+        $table->createTable();
+
+        $this->conn = new Connection();
+        $this->query = $this->conn->query();
     }
 
     /** @test */
-    public function can_create_query_object(): void
+    public function can_insert_row(): void
     {
-        $this->query = $this->pdo->query();
+        $insert = $this->query->insert(['test_string', 'test_int'], ['test val', 2])
+            ->into('test_table')
+            ->execute();
 
-        $this->assertInstanceOf(Query::class, $this->query);
+        $this->assertTrue($insert);
+    }
+
+    /** @test */
+    public function can_select_inserted_row(): void
+    {
+        $row = $this->query->select()
+            ->from('test_table')
+            ->where('test_int = 2')
+            ->execute();
+
+        $expected = [
+            'test_id' => '1',
+            'test_string' => 'test val',
+            'test_int' => '2',
+        ];
+
+        $this->assertEquals($expected, $row[0]);
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        Builder::destroy('test_table');
     }
 }
